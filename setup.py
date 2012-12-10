6553bac076b42f6f76b88173552042b49113618b
Mike Boers <mboers@keypics.com>
2012-12-10 15:37:30 -0800

    Add setup.py for installing on Travis for other tools

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+
+    name='uifutures',
+    version='0.1-dev',
+    description='Qt UI for background jobs.',
+    url='http://github.com/westernx/uifutures',
+    
+    packages=['uifutures'],
+    
+    author='Mike Boers',
+    author_email='uifutures@mikeboers.com',
+    license='BSD-3',
+    
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    
+)
