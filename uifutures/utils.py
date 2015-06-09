c703bcdbd4cdf066313886e1bdca6967495702ef
Mike Boers <westernx@mikeboers.com>
2015-06-09 10:51:31 -0700

    Use uitools' new notifications

--- uifutures/utils.py
+++ uifutures/utils.py
@@ -50,23 +50,6 @@ def notify(message, title=None, app_name=None, sticky=False, icon=None):
     if title is None:
         title = 'Job Queue'
     
-    if sys.platform.startswith('darwin'):
-        argv = ['growlnotify', '--message', message]
-        if app_name:
-            argv.extend(('--name', app_name))
-        if title:
-            argv.extend(('--title', title))
-        if sticky:
-            argv.append('--sticky')
-        if icon:
-            argv.extend(('--image', icon))
-    else:
-        argv = ['notify-send']
-        if sticky:
-            argv.extend(['-t', '3600000'])
-        if icon:
-            argv.extend(('--icon', icon))
-        argv.extend([title, message])
-    
-    call(argv)
+    from uitools.notifications import Notification
+    Notification(title, message).send()
 
